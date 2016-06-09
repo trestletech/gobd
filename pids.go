f726384535f42a326232acc3f92891f888675fef
trestletech <jeff.allen@trestletechnology.net>
2016-06-08 20:53:47 -0500

    More functions and tests.

--- pids.go
+++ pids.go
@@ -1,6 +1,36 @@
 package main
 
+// [1 3 4 5 6 7 11 12 13 14 15 16 17 19 21 28 31 32 33 44 46 47 48 49 50 51 52 60 64 65 66 67 68 69 70 71 73 74 76
+var monitorStatusPID int = 1
+var fuelStatusPID int = 3
 var engineLoadPID int = 4
+var coolantTempPID int = 5
+
+/*var shortTermFuelTrim1PID int = 6
+var longTermFuelTrim1PID int = 7
+var shortTermFuelTrim2PID int = 8
+var longTermFuelTrim2PID int = 9
+var intakeManifoldAbsPressurePID int = 11
+*/
+var engineRPMPID int = 12
+var speedPID int = 13
+
+//var timingAdvancePID int = 14
+//var intakeAirTempPID int = 15
+
+type monitorStatus struct{}
+
+func (o *OBD) GetMonitorStatus() (monitorStatus, error) {
+	//TODO
+	return monitorStatus{}, nil
+}
+
+type fuelStatus struct{}
+
+func (o *OBD) GetFuelStatus() (fuelStatus, error) {
+	//TODO
+	return fuelStatus{}, nil
+}
 
 func (o *OBD) GetEngineLoad() (float64, error) {
 	if !includes(o.pids, engineLoadPID) {
@@ -8,4 +38,28 @@ func (o *OBD) GetEngineLoad() (float64, error) {
 	}
 	val, err := o.currentInt(engineLoadPID)
 	return float64(val) / 2.55, err
+}
+
+func (o *OBD) GetCoolantTemp() (int, error) {
+	if !includes(o.pids, coolantTempPID) {
+		return 0, pidNotSupportedError(coolantTempPID)
+	}
+	val, err := o.currentInt(coolantTempPID)
+	return val - 40, err
+}
+
+func (o *OBD) GetRPM() (float64, error) {
+	if !includes(o.pids, engineRPMPID) {
+		return float64(0), pidNotSupportedError(engineRPMPID)
+	}
+	val, err := o.currentInt(engineRPMPID)
+	return float64(val) / 4, err
+}
+
+func (o *OBD) GetSpeed() (int, error) {
+	if !includes(o.pids, speedPID) {
+		return 0, pidNotSupportedError(speedPID)
+	}
+	val, err := o.currentInt(speedPID)
+	return val, err
 }
--- pids_test.go
+++ pids_test.go
@@ -39,5 +39,73 @@ func (ps *PidsSuite) TestEngineLoad(c *check.C) {
 	c.Assert(err, check.IsNil)
 	// Floats may not be identical. That's fine.
 	c.Check(math.Abs(l-255/2.55) < .00001, check.Equals, true)
+}
 
+func (ps *PidsSuite) TestCoolantTemp(c *check.C) {
+	ms := newMockSerial()
+	ms.addResponse("0100", "41 00 08")
+	ms.addResponse("0105", "41 05 29")
+
+	obd, err := NewDebugOBD(ms, c.Logf)
+	c.Assert(err, check.IsNil)
+
+	val, err := obd.GetCoolantTemp()
+	c.Assert(err, check.IsNil)
+	c.Check(val, check.Equals, 1)
+
+	ms.addResponse("0105", "41 05 FF")
+	val, err = obd.GetCoolantTemp()
+	c.Assert(err, check.IsNil)
+	c.Check(val, check.Equals, 215)
+
+	ms.addResponse("0105", "41 05 00")
+	val, err = obd.GetCoolantTemp()
+	c.Assert(err, check.IsNil)
+	c.Check(val, check.Equals, -40)
+}
+
+func (ps *PidsSuite) TestRPM(c *check.C) {
+	ms := newMockSerial()
+	ms.addResponse("0100", "41 00 00 10")
+	ms.addResponse("010c", "41 0c 55 55")
+
+	obd, err := NewDebugOBD(ms, c.Logf)
+	c.Assert(err, check.IsNil)
+
+	val, err := obd.GetRPM()
+	c.Assert(err, check.IsNil)
+	c.Check(math.Abs(val-5461.25) < 0.00001, check.Equals, true)
+
+	ms.addResponse("010c", "41 0c 00 00")
+	val, err = obd.GetRPM()
+	c.Assert(err, check.IsNil)
+	c.Check(math.Abs(val) < 0.00001, check.Equals, true)
+
+	ms.addResponse("010c", "41 0c FF FF")
+	val, err = obd.GetRPM()
+	c.Assert(err, check.IsNil)
+	c.Check(math.Abs(val-16383.75) < 0.00001, check.Equals, true)
+}
+
+func (ps *PidsSuite) TestSpeed(c *check.C) {
+	ms := newMockSerial()
+	ms.addResponse("0100", "41 00 00 08")
+	ms.addResponse("010d", "41 0d 55")
+
+	obd, err := NewDebugOBD(ms, c.Logf)
+	c.Assert(err, check.IsNil)
+
+	val, err := obd.GetSpeed()
+	c.Assert(err, check.IsNil)
+	c.Check(val, check.Equals, 85)
+
+	ms.addResponse("010d", "41 0d 00")
+	val, err = obd.GetSpeed()
+	c.Assert(err, check.IsNil)
+	c.Check(val, check.Equals, 0)
+
+	ms.addResponse("010d", "41 0d FF")
+	val, err = obd.GetSpeed()
+	c.Assert(err, check.IsNil)
+	c.Check(val, check.Equals, 255)
 }
